d12628d0ee78e798ccd8fa367bf5472b820b8141
Bansook Nam <bs.nam@navercorp.com>
2019-03-14 22:54:28 +0900

    add ball model

---
+++ app/src/main/java/com/nam/android/svc/lotto/vo/Ball.kt
@@ -0,0 +1,11 @@
+package com.nam.android.svc.lotto.vo
+
+import androidx.annotation.ColorInt
+
+data class Ball
+@JvmOverloads
+constructor(
+    val number: Int,
+    @ColorInt val color: Int,
+    var isSelected: Boolean = false
+)
---
+++ app/src/main/java/com/nam/android/svc/lotto/vo/BallPool.kt
@@ -0,0 +1,25 @@
+package com.nam.android.svc.lotto.vo
+
+import android.graphics.Color
+
+/**
+ * @author bs.nam@navercorp.com
+ */
+object BallPool {
+    
+    fun createBalls(): List<Ball> {
+        val list = ArrayList<Ball>()
+        for (i in 1..45) {
+            list.add(Ball(i, getRandomColor(), false))
+        }
+        return list
+    }
+
+    private fun getRandomColor(): Int {
+        return Color.rgb(
+            Math.floor(Math.random() * 128).toInt() + 64,
+            Math.floor(Math.random() * 128).toInt() + 64,
+            Math.floor(Math.random() * 128).toInt() + 64
+        )
+    }
+}
